31d2037550f7ae1c357f750aa486d6eacc3e4171
Jeff Wong <42687471+jeff-yc-wong@users.noreply.github.com>
2025-01-17 18:24:16 -1000

    added a way to specify cpu speed for each docker worker

--- docker/build.sh
+++ docker/build.sh
@@ -1,1 +1,5 @@
-docker build --target builder -t parsl-worker .
+if [ $# -eq 1 ]; then
+  docker build --target builder -t parsl-worker --build-arg CACHEBUST=$(date +%s) .
+else
+  docker build --target builder -t parsl-worker .
+fi
